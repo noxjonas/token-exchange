6d2d0be447d1711548da8004db6203007c97674c
noxjonas <jonas.kondr@gmail.com>
2022-10-06 21:05:30 +0100

    add release build

--- build.sh
+++ build.sh
@@ -4,24 +4,9 @@ archs=(amd64 arm64)
 
 for os in ${oses[@]}
 do
+  extension=$([ $os == "windows" ] && echo ".exe" || echo "")
   for arch in ${archs[@]}
   do
-    env GOOS=${os} GOARCH=${arch} go build -o dist/tx_${os}_${arch}
+    env GOOS=${os} GOARCH=${arch} go build -o dist/tx_${os}_${arch}${extension}
   done
 done
-
-
-#
-#      # Runs a set of commands using the runners shell
-#      - name: Run a multi-line script
-#        run: |
-#          oses=(windows darwin linux)
-#          archs=(amd64 arm64)
-#
-#          for os in ${oses[@]}
-#          do
-#            for arch in ${archs[@]}
-#            do
-#              env GOOS=${os} GOARCH=${arch} go build -o dist/tx_${os}_${arch}
-#            done
-#          done
